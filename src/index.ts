87133cfcb9d1e9b7826d7bca7d0c5c9940b85e14
Bhupendra Singh <bhupi29491@gmail.com>
2024-04-02 18:51:36 +0300

    Refactored the project to generate the report

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
-import { MatchResult } from './MatchResult';
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Liverpool');
 
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (
-    (match[1] == 'Man United' && match[5] == MatchResult.HomeWin) ||
-    (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
-  ) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
